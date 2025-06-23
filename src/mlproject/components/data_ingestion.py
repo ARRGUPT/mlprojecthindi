e94b1c3fbdb97f26cc7d10528898f5c1076aa57a
Aryan Gupta <guptafamily3005@gmail.com>
2025-06-23 23:26:52 +0530

    Data Ingestion

---
+++ src/mlproject/components/data_ingestion.py
@@ -0,0 +1,51 @@
+# database(mysql/mongodb/postgres) ---> read data(in utils.py) ---> train test split ---> output(having dataset in train test split format)
+
+import os                                             # for current working directory
+import sys                                            # to handle CustomException
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
+from mlproject.exception import CustomException
+from mlproject.logger import logging
+import pandas as pd
+from mlproject.utils import read_sql_data
+
+from sklearn.model_selection import train_test_split
+
+
+from dataclasses import dataclass                     # for input parameters initialization
+
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str = os.path.join("artifacts","train.csv")
+    test_data_path:str = os.path.join("artifacts","test.csv")
+    raw_data_path:str = os.path.join("artifacts","data.csv")
+    
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+        
+    def initiate_data_ingestion(self):
+        try:
+            #reading data from database
+            df = read_sql_data()
+            logging.info("Reading completed from mysql database")
+            
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            df.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            df.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info("Data ingestion completed")
+            
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+            )
+            
+            
+            
+
+        except Exception as e:
+            raise CustomException(e, sys)
---
+++ src/mlproject/utils.py
@@ -0,0 +1,38 @@
+# reading of data from database will be doen here
+
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
+from mlproject.exception import CustomException
+from mlproject.logger import logging
+import pandas as pd
+from dotenv import load_dotenv
+import pymysql
+
+load_dotenv()
+
+host = os.getenv('host')
+user = os.getenv('user')
+password = os.getenv('password')
+db = os.getenv('db')
+
+def read_sql_data():                                # will return dataframe
+    logging.info("Reading SQL database started")
+    
+    try:
+        mydb = pymysql.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=db
+        )
+        logging.info("Connected to the mysql database successfully", mydb)
+        
+        df = pd.read_sql_query("SELECT * FROM your_table_name", mydb)  # replace 'your_table_name' with actual table name
+        print(df.head())
+        
+        return df
+    
+        
+    except Exception as e:
+        raise CustomException(e, sys)
